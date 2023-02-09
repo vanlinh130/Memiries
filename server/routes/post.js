ea34fafa4e03e8b6498c210fb5986aa8ef2679a4
vanlinh1330 <levanlinh1330@gamil.com>
2023-02-09 10:07:17 +0700

    server

---
+++ server/routes/post.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import { getPost, getPosts, createPost, updatePost, deletePost, likePost, commentPost, getPostsBySearch } from '../controllers/post.js';
+
+const router = express.Router();
+import auth from '../middleware/auth.js';
+
+// http://localhost:5000/posts
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
+
+export default router;
